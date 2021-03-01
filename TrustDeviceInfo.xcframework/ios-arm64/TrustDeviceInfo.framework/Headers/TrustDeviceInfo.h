091f6526a5aded31cb0b034a8140094ff2f09677
Kevin Torres Montenegro <kevin.johan.yerel@gmail.com>
2021-03-01 16:54:22 -0300

    Added framework

---
+++ TrustDeviceInfo.xcframework/ios-arm64/TrustDeviceInfo.framework/Headers/TrustDeviceInfo.h
@@ -0,0 +1,19 @@
+//
+//  TrustDeviceInfo.h
+//  TrustDeviceInfo
+//
+//  Created by Diego Villouta Fredes on 8/27/18.
+//  Copyright © 2018 Jumpitt Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TrustDeviceInfo.
+FOUNDATION_EXPORT double TrustDeviceInfoVersionNumber;
+
+//! Project version string for TrustDeviceInfo.
+FOUNDATION_EXPORT const unsigned char TrustDeviceInfoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrustDeviceInfo/PublicHeader.h>
+
+
